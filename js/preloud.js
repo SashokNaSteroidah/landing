65ab17ac80bcfe601eb3d0a9bf6931a1031228ad
Nika <pavliushin.nikita@mail.ru>
2023-03-06 09:57:39 +0300

    Delete JS

--- js/preloud.js
+++
@@ -1,6 +0,0 @@
-$(window).on('load', function () {
-    var $preloader = $('#p_prldr'),
-        $svg_anm = $preloader.find('.svg_anm');
-    $svg_anm.delay(1500).fadeOut("slow");
-    $preloader.delay(1500).fadeOut('slow');
-});
